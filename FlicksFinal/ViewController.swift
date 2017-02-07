c8aa2b7fd84cad50af030ed4c7870739ea19b74c
Youssef <yelabd@purdue.edu>
2017-02-06 20:38:51 -0500

    Initial Commit

---
+++ FlicksFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlicksFinal
+//
+//  Created by Youssef Elabd on 2/6/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
